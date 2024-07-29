c54388b0a151d45f512c26870014bd674560c939
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2024-07-29 20:48:04 -0300

    chore: removed github foulder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
-
 import reactLogo from './assets/react.svg';
 
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -14,18 +10,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Boilerplate | Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((c) => c + 1)} type="button">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Lucas Korz - Portfólio</h1>
+      <div className="card">Em breve...</div>
     </>
   );
 }
